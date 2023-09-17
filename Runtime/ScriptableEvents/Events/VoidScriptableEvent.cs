64d47071278fea283ac5cbd91e1d9ef4a6047fb9
Luke <lukebradaric@gmail.com>
2023-09-16 20:16:54 -0700

    override voidscriptableevent raise method

--- Runtime/ScriptableEvents/Events/VoidScriptableEvent.cs
+++ Runtime/ScriptableEvents/Events/VoidScriptableEvent.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 namespace TinyTools.ScriptableEvents
 {
     [CreateAssetMenu(menuName = "TinyTools/ScriptableEvents/VoidScriptableEvent")]
-    public class VoidScriptableEvent : BaseScriptableEvent<Type> { }
+    public class VoidScriptableEvent : BaseScriptableEvent<Type>
+    {
+        public void Raise()
+        {
+            base.Raise(null);
+        }
+    }
 }
