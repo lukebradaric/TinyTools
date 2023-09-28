576e9fe97990c4416fce574487a9eb1cf376380c
Luke <lukebradaric@gmail.com>
2023-09-28 11:27:09 -0700

    more extensions / generics
    - list extensions
    - scriptableobject extensions
    - weighted list generic

---
+++ Runtime/Extensions/ScriptableObjectExtensions.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace TinyTools
+{
+    public static class ScriptableObjectExtensions
+    {
+        /// <summary>
+        /// Returns a clone of the ScriptableObject
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="scriptableObject"></param>
+        /// <returns></returns>
+        public static T GetClone<T>(this T scriptableObject) where T : ScriptableObject
+        {
+            if (scriptableObject == null)
+            {
+                Debug.LogError($"ScriptableObject was null. Returning default {typeof(T)} object.");
+                return (T)ScriptableObject.CreateInstance(typeof(T));
+            }
+
+            T instance = Object.Instantiate(scriptableObject);
+            instance.name = scriptableObject.name; // remove (Clone) from name
+            return instance;
+        }
+    }
+}
